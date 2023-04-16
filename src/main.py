24759ef60048cbb6a7ead329dfb4ef6b97021ac5
AllanCapistrano <allan.capistrano3@gmail.com>
2023-04-16 20:17:59 -0300

    chore: move main file and using HandDetector class

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from time import time
+
+import cv2
+from handDetector import HandDetector
+
+WEBCAM_INDEX = 0
+
+def main():
+    previous_time = 0
+    current_time = 0
+
+    hand_detector = HandDetector()
+
+    webcam = cv2.VideoCapture(WEBCAM_INDEX)
+
+    while(True):
+        success, image = webcam.read()
+
+        if(success):
+            hand_detector.process_image(image)
+            image_with_landmarks = hand_detector.draw_landmarks()
+            hands = hand_detector.find_positions()
+
+            if(len(hands) > 0):
+                print(hands[0]["landmarks"][6])
+
+            current_time = time()
+            fps = 1/(current_time - previous_time)
+            previous_time = current_time
+
+            cv2.putText(image_with_landmarks, f"FPS: {int(fps)}", (10, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 3)
+
+            cv2.imshow("Webcam", image_with_landmarks)
+            cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
