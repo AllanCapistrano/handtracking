3a8b1f03c210ed039475960592936ba70cfb62cf
AllanCapistrano <allan.capistrano3@gmail.com>
2023-04-19 13:03:55 -0300

    chore: add event to show fps

--- src/main.py
+++ src/main.py
@@ -6,16 +6,20 @@ from handDetector import HandDetector
 WEBCAM_INDEX = 0
 
 def main():
-    fps_start_time = 0
+    fps_start_time: float = 0
 
     hand_detector = HandDetector()
 
     webcam = cv2.VideoCapture(WEBCAM_INDEX)
 
+    fps_flag: bool = True
+
     while(True):
         success, frame = webcam.read()
 
         if(success):
+            key = cv2.waitKey(1)
+
             hand_detector.process_image(frame)
             image_with_landmarks = hand_detector.draw_landmarks()
             hands = hand_detector.find_positions()
@@ -23,17 +27,19 @@ def main():
             if(len(hands) > 0):
                 print(hands[0]["landmarks"][6])
 
-            fps_end_time = time()
-            fps = 1/(fps_end_time - fps_start_time)
-            fps_start_time = fps_end_time
+            fps_end_time: float = time()
+            fps: float = 1/(fps_end_time - fps_start_time)
+            fps_start_time: float = fps_end_time
 
-            cv2.putText(image_with_landmarks, f"FPS: {int(fps)}", (10, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 3)
+            if(key == 81 or key == 113): # q ou Q
+                break
+            elif(key == 80 or key == 112): # p ou P
+                fps_flag = not fps_flag
+
+            if(fps_flag):
+                cv2.putText(image_with_landmarks, f"FPS: {int(fps)}", (10, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 3)
 
             cv2.imshow("Webcam", image_with_landmarks)
-            key = cv2.waitKey(1)
-
-            if(key == 81 or key == 113):
-                break
     
     webcam.release()
 
